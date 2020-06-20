d3d8435718fa0201a2bac6c30ae38ce64d5ff7b9
gy5461 <1900269631@qq.com>
2020-06-20 23:02:42 +0800

    进一步封装了MongoDB

--- application/library/Db/Mongodb.php
+++ application/library/Db/Mongodb.php
@@ -1,7 +1,8 @@
 <?php
 /**
- * mongodb 数据库操作封装
+ * @name Db_Mongodb
  * @author guoyi
+ * @desc MongoDB数据库操作封装
  */
 class Db_Mongodb {
 
@@ -10,6 +11,12 @@ class Db_Mongodb {
 
     protected $manager = null;
     protected static $connection = [];
+
+    public $fields = [];
+    protected $db = 'student_competition_system';
+    public $collection = '';
+
+    protected $primary_key;
 
     /**
      * 禁止外部克隆
@@ -52,6 +59,97 @@ class Db_Mongodb {
         self::$connection[$this->host][$this->port] = new MongoDB\Driver\Manager(
             $uri
         );
+    }
+
+    /**
+     * 插入数据
+     * @param  array  $arr 插入的数据 ['name' => 'admin','sex' => '0']
+     * @return bool
+     */
+    public function insert(array $arr){
+        //若插入数组为空
+        if(empty($arr))
+            throw new Exception('Insert data is empty.');
+
+        $bulk = new MongoDB\Driver\BulkWrite;
+
+        //未定义字段
+        if(empty($this->fields))
+            throw new Exception('Please defiend the fields first.');
+
+        //将字段默认值置为arr设定值
+        foreach ($this->fields as $field => &$value) {
+            if(isset($arr[$field]))
+                $value = $arr[$field];
+        }
+        $newFields = $this->fields;
+
+        //插入新字段并获取_id
+        $_id = Tools::object_array($bulk->insert($newFields))['oid'];
+        $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
+        $result = $this->manager->executeBulkWrite("{$this->db}.{$this->collection}", $bulk, $writeConcern);
+        //获取本次成功插入条数
+        $isSuccess = (bool) $result->getInsertedCount();
+        //若插入成功返回_id
+        return $isSuccess === true ? $_id : false;
+    }
+
+    /**
+     * 删除数据
+     * @param  array  $match   匹配条件 参考查询、修改匹配条件语法
+     * @param  array  $options limit 为0 只删除符合条件的一条记录；为1则删除所有匹配记录
+     * @return [type]          [description]
+     */
+    public function delete(array $match, $options = ['limit'=>0]){
+        //若传入匹配条件为空
+        if(empty($match))
+        {
+            throw new Exception('Not safe for none match fields');
+        }
+
+        //若设置的limit值不为0/1
+        if(isset($options['limit']) && !in_array($options['limit'], [0,1]))
+        {
+            throw new Exception('Invalid limit option.');
+        }
+
+        $bulk = new MongoDB\Driver\BulkWrite;
+        $bulk->delete($match, $options);
+
+        $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
+
+        $result = $this->manager->executeBulkWrite("{$this->db}.{$this->collection}", $bulk, $writeConcern);
+        $isSuccess = $result->getDeletedCount();
+
+        return (bool)$isSuccess;
+    }
+
+    /**
+     * 原生语句执行
+     * @param array $cmd
+     * @return mixed
+     */
+    protected function execute(array $cmd) {
+        $cmd = new MongoDB\Driver\Command($cmd);
+        return $this->manager->executeCommand($this->db, $cmd);
+    }
+
+    /**
+     * 查询记录总量
+     * @param array $filter
+     * @param array $options
+     * @return int
+     */
+    public function count($filter = [], $options = []){
+        $query = new MongoDB\Driver\Query($filter, $options);
+        $cmd = [
+            'count' => $this->collection,
+            'query' => $query
+        ];
+        $cursor = $this->execute($cmd);
+        $info = $cursor->toArray();
+        $count = $info[0]->n;
+        return (int)$count;
     }
 }
 ?>
