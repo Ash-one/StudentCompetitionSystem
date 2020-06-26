795f4113fe26970fe81c44d900f96a1b34e68134
Kuroki <risakirisame@qq.com>
2020-06-26 16:34:26 +0800

    update ExcelPaser

--- application/library/ExcelParser.php
+++ application/library/ExcelParser.php
@@ -31,41 +31,82 @@ class ExcelParser
                 echo "sex_error";
             }
 
-            Dao_SchoolModel::getInstance()->update([ 'school_name'=>$sheetData[$i]["E"]], ['school_name'=>$sheetData[$i]["E"]], ['multi'=>false,'upsert'=>true]);
+            // TODO: $sheetData[$i]["C"] 年级的转换
+
+            // 插入 school 基础信息
+            Dao_SchoolModel::getInstance()->insert(['school_name'=>$sheetData[$i]["E"]]);
             $school_id = Dao_SchoolModel::getInstance()->queryOne(['school_name'=>$sheetData[$i]["E"]])["_id"];
-//            echo $school_id."   ";
-//            $student_doc = [ 'student_name'=>$sheetData[$i]["A"], 'student_id'=>$sheetData[$i]["B"], 'student_grade'=>$sheetData[$i]["C"], 'student_sex'=>$sheetData[$i]["D"]];
-//
-//            $student_doc2 = $student_doc + ['$addToSet'=>['school_object_id'=>$school_id]];
-//
-//            print_r($student_doc2) ;
-//            echo "\n";
-//            print_r($student_doc) ;
 
-            Dao_StudentModel::getInstance()->update([ 'student_name'=>$sheetData[$i]["A"]], ['student_name'=>$sheetData[$i]["A"]], ['multi'=>false,'upsert'=>true]);
-//            $student_id = Dao_StudentModel::getInstance()->queryOne(['student_name'=>$sheetData[$i]["A"]])["_id"];
-//            echo $student_id."  \n";
-//            Dao_CompetitionModel::getInstance()->update(['competition_name'=>$sheetData[$i]["H"], 'competition_start_time'=>$sheetData[$i]["I"], 'competition_end_time'=>$sheetData[$i]["J"]],['competition_name'=>$sheetData[$i]["H"], 'competition_start_time'=>$sheetData[$i]["I"], 'competition_end_time'=>$sheetData[$i]["J"]],['multi'=>false,'upsert'=>true]);
-//            $competition_id = Dao_CompetitionModel::getInstance()->queryOne(['competition_name'=>$sheetData[$i]["H"]])["_id"];
-//            Dao_MatchModel::getInstance()->update(['match_name'=>$sheetData[$i]["F"], 'match_time'=>$sheetData[$i]["G"], 'match_belong_competition'=>$competition_id],['match_name'=>$sheetData[$i]["F"], 'match_time'=>$sheetData[$i]["G"], 'match_belong_competition'=>$competition_id], ['multi'=>false,'upsert'=>true]);
-//            $match_id = Dao_StudentModel::getInstance()->queryOne(['match_name'=>$sheetData[$i]["F"]])["_id"];
-//            Dao_AwardModel::getInstance()->update(['competition_object_id'=>$competition_id,'match_object_id'=>$match_id,'award_type'=>$sheetData[$i]["L"],'student_object_id'=>$student_id,'school_object_id'=>$school_id,'award_rank'=>$sheetData[$i]["K"]],['competition_object_id'=>$competition_id,'match_object_id'=>$match_id,'award_type'=>$sheetData[$i]["L"],'student_object_id'=>$student_id,'school_object_id'=>$school_id,'award_rank'=>$sheetData[$i]["K"]], ['multi'=>false,'upsert'=>true]);
-//            $Award_id = Dao_StudentModel::getInstance()->queryOne(['match_object_id'=>$match_id, 'student_object_id'=>$student_id])["_id"];
-//
-//            Dao_StudentModel::getInstance()->update(['$addToSet'=>['student_competition_details'=>$competition_id]]);
-//            Dao_StudentModel::getInstance()->update(['$addToSet'=>['student_award_details'=>$Award_id]]);
-//
-//            Dao_SchoolModel::getInstance()->update(['$addToSet'=>['school_competition_details'=>$competition_id]]);
-//            Dao_SchoolModel::getInstance()->update(['$addToSet'=>['school_award_details'=>$Award_id]]);
-//            Dao_SchoolModel::getInstance()->update(['$addToSet'=>['school_students'=>$student_id]]);
-//
-//            Dao_MatchModel::getInstance()->update(['$addToSet'=>['match_student_details'=>$student_id]]);
-//
-//            Dao_CompetitionModel::getInstance()->update(['$addToSet'=>['competition_participating_schools'=>$school_id]]);
-//            Dao_CompetitionModel::getInstance()->update(['$addToSet'=>['competition_participating_students'=>$student_id]]);
-//            Dao_CompetitionModel::getInstance()->update(['$addToSet'=>['competition_award_details'=>$Award_id]]);
-//            Dao_CompetitionModel::getInstance()->update(['$addToSet'=>['competition_match_items'=>$match_id]]);
+            // 插入 student 基础信息
+            $student_id = $sheetData[$i]["B"];
+            if (Dao_StudentModel::getInstance()->queryOne(['student_id' => $student_id]) == null) {
+                Dao_StudentModel::getInstance()->insert([
+                        'student_name'=>$sheetData[$i]["A"], 
+                        'student_id' => $student_id,
+                        'student_grade' => $sheetData[$i]["C"],
+                        'student_sex' => $sheetData[$i]["D"],
+                    ]
+                );
+            }
+            $student_oid = Dao_StudentModel::getInstance()->queryOne(['student_id'=>$sheetData[$i]["B"]])["_id"];
+            
+            // 插入 competiton 基础信息
+            $competition_name = $sheetData[$i]["H"];
+            $competition_sTime = $sheetData[$i]["I"];
+            if (Dao_CompetitionModel::getInstance()->queryOne(['competition_name'=>$competition_name, 'competition_start_time'=>$competition_sTime]) == null) {
+                Dao_CompetitionModel::getInstance()->insert([
+                        'competition_name'=>competition_name,
+                        'competition_start_time'=>competition_sTime, 
+                        'competition_end_time'=>$sheetData[$i]["J"]
+                    ]
+                );
+            }          
+            $competition_id = Dao_CompetitionModel::getInstance()->queryOne(['competition_name'=>$competition_name, 'competition_start_time'=>$competition_sTime])["_id"];
 
+            // 插入 match 基础信息
+            $match_name = $sheetData[$i]["F"];
+            if (Dao_MatchModel::getInstance()->queryOne(['match_name'=>$match_name, 'match_belong_competition'=>$competition_id]) == null) {
+                Dao_MatchModel::getInstance()->insert([
+                        'match_name'=>$match_name,
+                        'match_time'=>$sheetData[$i]["G"], 
+                        'match_belong_competition'=>$competition_id
+                    ]
+                );
+            }
+            $match_id = Dao_MatchModel::getInstance()->queryOne(['match_name'=>$match_name, 'match_belong_competition'=>$competition_id])['_id'];
+
+            // 插入 award 基础信息
+            Dao_AwardModel::getInstance()->insert([
+                    'competition_object_id'=>$competition_id,
+                    'match_object_id'=>$match_id, 
+                    'award_type'=>$sheetData[$i]["L"],
+                    'student_object_id'=>$student_oid,
+                    'school_object_id'=>$school_id,
+                    'award_rank'=>$sheetData[$i]["K"]
+                ]
+            );
+            $award_id = Dao_AwardModel::getInstance()->queryOne([
+                    'competition_object_id'=>$competition_id,
+                    'match_object_id'=>$match_id, 
+                    'student_object_id'=>$student_oid,
+                ]
+            )['_id'];
+
+            // 插入关联信息
+            Dao_SchoolModel::getInstance()->update(['_id'=>$school_id], ['$addToSet'=>['school_competition_details'=>$competition_id]]);
+            Dao_SchoolModel::getInstance()->update(['_id'=>$school_id], ['$addToSet'=>['school_award_details'=>$award_id]]);
+            Dao_SchoolModel::getInstance()->update(['_id'=>$school_id], ['$addToSet'=>['school_students'=>$student_oid]]);
+
+            Dao_StudentModel::getInstance()->update(['_id'=>$student_oid], ['$addToSet'=>['student_competition_details'=>$competition_id]]);
+            Dao_StudentModel::getInstance()->update(['_id'=>$student_oid], ['$addToSet'=>['student_award_details'=>$award_id]]);
+            Dao_StudentModel::getInstance()->update(['_id'=>$student_oid], ['$addToSet'=>['student_match_details'=>$match_id]]);
+            
+            Dao_CompetitionModel::getInstance()->update(['_id'=>$competition_id], ['$addToSet'=>['competition_participating_schools'=>$school_id]]);
+            Dao_CompetitionModel::getInstance()->update(['_id'=>$competition_id], ['$addToSet'=>['competition_participating_students'=>$student_oid]]);
+            Dao_CompetitionModel::getInstance()->update(['_id'=>$competition_id], ['$addToSet'=>['competition_award_details'=>$award_id]]);
+            Dao_CompetitionModel::getInstance()->update(['_id'=>$competition_id], ['$addToSet'=>['competition_match_items'=>$match_id]]);
+  
+            Dao_MatchModel::getInstance()->update(['$addToSet'=>['match_student_details'=>$student_oid]]);
         }
 
     }
--- application/models/Dao/School.php
+++ application/models/Dao/School.php
@@ -26,13 +26,6 @@ class Dao_SchoolModel extends Db_Mongodb implements Service_ISchoolModel {
 //            $this->insert(["school_name"=>"中国传媒大学", "school_competition_details"=>[], "school_award_details"=>[],
 //                "school_students"=>[]]);
 //        }
-
-        //查询结果
-        // $filter = [];
-        // $result = $this->query($filter);
-        // foreach ($result as $document) {
-        //     print_r($document);
-        // }
     }
 
     /**
